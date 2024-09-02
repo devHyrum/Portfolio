93bd35bb727c562da1b0f59c03c2ec5df7185134
holivera159 <holivera0730@gmail.com>
2024-09-02 00:58:00 -0300

    Curriculum hecho

---
+++ README.md
@@ -0,0 +1,42 @@
+# Portfolio Pessoal - Hyrum Spencer Olivera Fernandez
+![mobileDesign](./public/mobileDemo.png)
+
+Bem-vindo ao meu portfólio pessoal! Este é um projeto que exibe minha trajetória profissional, habilidades, experiência e projetos. O site foi desenvolvido usando **HTML**, **SCSS**, e **JavaScript**.
+
+## Tecnologias Utilizadas
+
+- **HTML5**: Para estruturar o conteúdo.
+- **SCSS (Sass)**: Pré-processador CSS para organizar e facilitar o desenvolvimento dos estilos.
+- **JavaScript**: Para adicionar interatividade ao site.
+- **Google Fonts**: Integração com fontes externas.
+
+## Estrutura do Projeto
+
+O projeto é organizado em várias seções:
+
+1. **Apresentação**: Uma breve introdução sobre mim, com links para minhas redes sociais e foto de perfil.
+2. **Formação Acadêmica**: Detalhes sobre minhas qualificações e cursos.
+3. **Experiência Profissional**: Informações sobre minhas experiências de trabalho e empresas pelas quais passei.
+4. **Projetos**: Lista de projetos desenvolvidos com integração a uma API para exibir meus repositórios do GitHub.
+5. **Fale Comigo**: Formulário de contato e links para redes sociais.
+
+## Como Rodar o Projeto
+
+Para rodar o projeto localmente, siga os passos abaixo:
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/devHyrum/portfolio-pessoal.git
+   ```
+2. Navegue até o diretório do projeto:
+   ```bash
+   cd portfolio-pessoal
+   ```
+3. Certifique-se de que você tem um servidor local para visualizar o projeto (pode usar o Live Server no VSCode, por exemplo).
+4. Abra o arquivo index.html no seu navegador ou sirva o projeto em um servidor local.
+
+## Como Contribuir
+Se você deseja contribuir com este projeto, fique à vontade para abrir issues e enviar pull requests. Sugestões de melhorias são sempre bem-vindas!
+
+## Licença
+Este projeto está licenciado sob a licença MIT. Sinta-se à vontade para utilizá-lo e adaptá-lo para seus próprios fins.
--- main.js
+++ main.js
@@ -14,25 +14,35 @@ if (form) {
 }
 
 
-const username = 'devHyrum'; // Seu nome de usuário do GitHub
+const username = 'devHyrum';
 const reposContainer = document.querySelector('.projetos');
+const desiredRepos = [
+  'User-Management-and-Blogging-API',
+  'React-Accommodation-Finder',
+  'weather-application',
+  'Front-end-Authentication-System',
+  'Tailwind-News-Homepage',
+  'Back-end-Authentication-System'
+];
 fetch(`https://api.github.com/users/${username}/repos`)
   .then(response => response.json())
   .then(data => {
     if (data && Array.isArray(data)) {
       data.forEach(repo => {
-        const repoDiv = document.createElement('div');
-        repoDiv.classList.add('section-projetos');
-        repoDiv.innerHTML = `
-            <img src="https://raw.githubusercontent.com/devHyrum/Sunnyside-Agency-Landing-Page/master/public/Desktop-Design.png" alt="${repo.name}">
-            <h1>${repo.name}</h1>
-            <p>${repo.description}</p>
-            <div>
-                <button><a href="${repo.html_url}" target="_blank">REPÓSITORIO</a></button>
-                <button><a href="${repo.homepage}" target="_blank">VER DEMO</a></button>
-            </div>
-        `;
-        reposContainer.appendChild(repoDiv);
+        if (desiredRepos.includes(repo.name)) {
+          const repoDiv = document.createElement('div');
+          repoDiv.classList.add('section-projetos');
+          repoDiv.innerHTML = `
+              <img src="./scss/assets/images/githubWallpaper.jpg" alt="${repo.name}">
+              <h5>${repo.name}</h1>
+              <p>${repo.description}</p>
+              <div>
+                  <button><a href="${repo.html_url}" target="_blank">REPÓSITORIO</a></button>
+                  <button><a href="${repo.homepage}" target="_blank">VER DEMO</a></button>
+              </div>
+          `;
+          reposContainer.appendChild(repoDiv);
+        }
       });
     } else {
       console.error('A resposta da API não contém a estrutura esperada.', data);
